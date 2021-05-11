c76cd262f7b65c929f66b66d113ed836744fc388
vaibhavmittal1998 <vaibhav.mittal1998@gmail.com>
2021-05-11 18:36:28 +0530

    add reselect to the project and add cart selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import {toggleCartDropdown}  from '../../redux/cart/cart.actions';
+import {selectCartItemCount, selectCartItems}  from '../../redux/cart/cart.selector';
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import { connect } from 'react-redux';
@@ -8,16 +9,17 @@ import { cartItemCount } from "../../redux/cart/cart.utils";
 import './cart-icon.styles.scss';
 
 
-const CartIcon = ({toggleCartDropdown, cartItems}) => (
+const CartIcon = ({toggleCartDropdown, cartItems, cartItemCount}) => (
     <div className='cart-icon' onClick={toggleCartDropdown}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>{cartItemCount(cartItems)}</span>
+        <span className='item-count'>{cartItemCount}</span>
     </div>
 );
 
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser,
-    cartItems: state.cart.cartItems
+    cartItems: selectCartItems(state),
+    cartItemCount: selectCartItemCount(state)
 });
   
 const mapDispatchToProps = dispatch => ({
